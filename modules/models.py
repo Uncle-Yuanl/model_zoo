07a44503801997a7d35ea5da3b15a2c1dba82d6e
Uncle-Yuanl <1097304891@qq.com>
2022-02-14 19:08:03 +0800

    完成Transformer基类主体

--- modules/models.py
+++ modules/models.py
@@ -5,6 +5,8 @@ from tensorflow.keras import Model
 import tensorflow.keras as keras
 import tensorflow.keras.backend as K
 
+from .layers import *
+
 
 class Transformer(object):
     """模型基类
@@ -18,15 +20,15 @@ class Transformer(object):
         intermediate_size,  # FeedForward的隐层维度
         hidden_act,  # FeedForward隐层的激活函数
         dropout_rate=None,  # Dropout比例
-        attention_dropout_rate=None,  # Attention矩阵的Dropout比例
-        embedding_size=None,  # 是否指定embedding_size
+        attention_dropout_rate=None,  # Attention矩阵的Dropout比例？？？？
+        embedding_size=None,  # 是否指定embedding_size  # 指定了会怎么样，裁剪 or padding？？
         attention_head_size=None,  # Attention中V的head_size
         attention_key_size=None,  # Attention中Q,K的head_size
-        sequence_length=None,  # 是否固定序列长度
-        keep_tokens=None,  # 要保留的词ID列表
+        sequence_length=None,  # 是否固定序列长度  # 还有不固定的吗，怎么实现？？
+        keep_tokens=None,  # 要保留的词ID列表  # 干啥的？？  # embeddings矩阵根据ID选择并重构
         compound_tokens=None,  # 扩展Embedding
-        residual_attention_scores=False,  # Attention矩阵加残差
-        ignore_invalid_weights=False,  # 允许跳过不存在的权重
+        residual_attention_scores=False,  # Attention矩阵加残差  # 为什么要有，是有论文吗？？  # 有
+        ignore_invalid_weights=False,  # 允许跳过不存在的权重  # 如何跳过，然后随机初始化吗？？
         autoresize_weights=False,  # 自动变换形状不匹配的权重
         layers=None,  # 外部传入的Keras层
         prefix=None,  # 层名前缀
@@ -42,7 +44,7 @@ class Transformer(object):
         self.num_hidden_layers = num_hidden_layers
         self.num_attention_heads = num_attention_heads
         self.attention_head_size = attention_head_size or hidden_size // num_attention_heads
-        self.attention_key_size = attention_key_size or self.attention_head_size
+        self.attention_key_size = attention_key_size or self.attention_head_size  # 这里看出维度并没有完全要求一致？？
         self.intermediate_size = intermediate_size
         self.dropout_rate = dropout_rate or 0
         self.attention_dropout_rate = attention_dropout_rate or 0
@@ -55,12 +57,12 @@ class Transformer(object):
         self.position_bias = None
         self.attention_scores = None
         self.residual_attention_scores = residual_attention_scores
-        self.ignore_invalid_weights = ignore_invalid_weights
+        self.ignore_invalid_weights = ignore_invalid_weights  #
         self.autoresize_weights = autoresize_weights
         self.layers = {} if layers is None else layers
         self.prefix = prefix or ''
         self.name = name
-        self.built = False
+        self.built = False  # 表示模型是否已经完成build
 
     def build(
         self,
@@ -72,10 +74,14 @@ class Transformer(object):
         **kwargs
     ):
         """模型构建函数
-        attention_caches：为Attention的K,V的缓存序列字典，格式为
-                         {Attention层名: [K缓存, V缓存]}；
-        layer_norm_*系列参数：实现Conditional Layer Normalization时使用，
-                            用来实现以“固定长度向量”为条件的条件Bert。
+
+        parameters:
+        -----------
+        attention_caches: dict
+            为Attention的k, v的缓存序列字段，{Attention层名: [k缓存, v缓存]}
+        layer_norm_*: ??
+            实现Conditional Layer Normalization时使用，用来实现"固定长度向量"为条件的条件Bert
+            啥呀？？？？
         """
         if self.built:
             return None
@@ -158,6 +164,7 @@ class Transformer(object):
                     # 为什么只有两个，q没有？？？？
                     k_cache, v_cache = self.attention_caches[name]
                     k_name, v_name = name + '-Cached-Key', '-Cached-Value'
+                    # 已测，可以调用
                     k = Concatenate1D(name=k_name)([k_cache, inputs[1]])
                     v = Concatenate1D(name=v_name)([v_cache, inputs[2]])
                     inputs = inputs[:1] + [k, v] + inputs[3:]
@@ -211,6 +218,7 @@ class Transformer(object):
         elif not isinstance(inputs, list):
             inputs = [inputs]
 
+        # deepcopy
         inputs = inputs[:]
         if additional_input_layers is not None:
             if not isinstance(additional_input_layers, list):
@@ -218,10 +226,11 @@ class Transformer(object):
             inputs.extend(additional_input_layers)
 
         self.inputs = inputs
+        # -------------------提取第一个input的话是不是写错了？---------------------------
         if len(inputs) > 1:
             self.input = inputs
         else:
-            # 这是干嘛？？？？
+            # 这是干嘛？？保持维度吗？？
             self.input = inputs[0]
 
     def set_outputs(self, outputs):
@@ -267,8 +276,8 @@ class Transformer(object):
             ext_embeddings = []
             for item in self.compound_tokens:
                 if isinstance(item, list):
+                    # 权重weights都是1
                     item = (item, [1] * len(item))
-                    # 这是啥意思？？？？
                 ext_embeddings.append(np.average(embeddings[item[0]], 0, item[1]))
             embeddings = np.concatenate([embeddings, ext_embeddings], 0)
 
@@ -281,6 +290,109 @@ class Transformer(object):
             return checkpoint[name]
         else:
             return tf.train.load_variable(checkpoint, name)
+
+    def create_variable(self, name, value, dtype=None):
+        """创建一个变量
+        """
+        dtype = dtype or K.floatx()
+        return K.variable(
+            # 这里不是加了装饰器？应该不能再执行__call__()？？
+            self.initializer(value.shape, dtype), dtype, name
+        ), value
+
+    def variable_mapping(self):
+        """建立keras层与checkpoint的变量名之间的映射
+        """
+        return {}
+
+    def load_weights_from_checkpoint(self, checkpoint, mapping=None):
+        """根据mapping从checkpoint加载权重
+        """
+        # -----------------这个mapping是在哪里完成配置的？？------------------------
+        mapping = mapping or self.variable_mapping()
+        # 加上前缀
+        mapping = {self.prefixed(k): v for k, v in mapping.items()}
+        # 只保留self.layers中的层
+        mapping = {k: v for k, v in mapping.items() if k in self.layers}
+
+        weight_value_pairs = []
+        for layer, variables in mapping.items():
+            # layer: 层名， variables: checkpoint变量名
+            # ---------------------所以self.layers是怎么完成配置的？？----------------------------
+            layer = self.layers[layer]
+            weights, values = [], []
+
+            for w, v in zip(layer.trainable_weights, variables):  # 允许跳过不存在的权重
+                try:
+                    values.append(self.load_variable(checkpoint, v))
+                    weights.append(w)
+                except Exception as e:
+                    if self.ignore_invalid_weights:
+                        print('%s, but ignored.' % e.message)
+                    else:
+                        raise e
+
+            for i, (w, v) in enumerate(zip(weights, values)):
+                if v is not None:
+                    w_shape, v_shape = K.int_shape(w), v.shape
+                    if self.autoresize_weights and w_shape != v_shape:
+                        # ----------------为什么是对v进行操作-------------------
+                        v = orthogonally_resize(v, w_shape)
+                        if isinstance(layer, MultiHeadAttention):
+                            # -------------这个count是啥----------------
+                            count = 2
+                            if layer.use_bias:
+                                count += 2
+                            if layer.attention_scale and i < count:
+                                scale = 1.0 * w_shape[-1] / v_shape[-1]
+                                # 这里咋就4次方根
+                                v = v * scale**0.25
+                            if isinstance(layer, FeedForward):
+                                count = 1
+                                if layer.use_bias:
+                                    count += 1
+                                if self.hidden_act in ['relu', 'leaky_relu']:
+                                    count -= 2
+                                if i < count:
+                                    v *= np.sqrt(1.0 * w_shape[-1] / v_shape[-1])
+                                else:
+                                    # ----------------为什么这里选择第0维度--------------
+                                    v *= np.sqrt((1.0 * w_shape[0]) / v_shape[0])
+
+                    weight_value_pairs.append((w, v))
+        # ----------API学习，set完了模型的权重就更新了吗？？---------------
+        K.batch_set_value(weight_value_pairs)
+
+    def save_weights_as_checkpoint(self, filename, mapping=None, dtype=None):
+        """根据mapping将权重保存为checkpoint格式
+        """
+        mapping = mapping or self.variable_mapping()
+        mapping = {self.prefixed(k): v for k, v in mapping.items()}
+        mapping = {k: v for k, v in mapping.items() if k in self.layers}
+
+        with tf.Graph().as_default():
+            all_variables, all_values = [], []
+            for layer, variables in mapping.items():
+                layer = self.layers[layer]
+                values = K.batch_get_value(layer.trainable_weights)
+                for name, value in zip(variables, values):
+                    variable, value = self.create_variable(name, value, dtype)
+                    all_variables.append(variable)
+                    all_values.append(value)
+            # tensorflow 2.x 貌似已经没有了
+            # 直接使用tf.train.CheckpointManager以及Checkpoint类
+            with tf.Session() as sess:
+                K.batch_set_value(zip(all_variables, all_values))
+                saver = tf.train.Saver()
+                saver.save(sess, filename)3
+
+
+
+
+
+
+
+
 
 
 
