1d0376c4201f3813f162530a2966bdbf879b160d
Uncle-Yuanl <1097304891@qq.com>
2022-02-14 19:08:48 +0800

    添加Concatenate1D的单元测试

---
+++ modules/layer_tests.py
@@ -0,0 +1,40 @@
+import unittest
+
+from .layers import *
+
+class TestConcatenate1D(unittest.TestCase):
+    a = tf.constant([[[1, 2, 3], [4, 5, 6], [7, 8, 9]]])
+    b = tf.constant([[[10, 20, 30], [40, 50, 60], [70, 80, 90]]])
+    c1d = Concatenate1D(name='test')
+    def test3d(self):
+        """这个应该要通过"""
+        c3 = self.c1d((self.a, self.b))
+        print(c3)
+
+    def test2d(self):
+        """这是基础使用, 也只有这个通过了"""
+        c2 = self.c1d((self.a[0], self.b[0]))
+        print(c2)
+
+    def test1d(self):
+        """axis=1导致无法成功，axis=0可以的"""
+        c1 = self.c1d((self.a[0][0], self.b[0][0]))
+        print(c1)
+
+
+
+
+
+if __name__ == '__main__':
+    # unittest.main()
+    # 指定类运行单元测试
+    class2run = [TestConcatenate1D]
+
+    loader = unittest.TestLoader()
+    suits = []
+    for cls in class2run:
+        suits.append(loader.loadTestsFromTestCase(cls))
+    suits = unittest.TestSuite(suits)
+
+    runner = unittest.TestRunner()
+    results = runner.run(suits)
