455a2fd4ac5562a922f29e68de2f4e1fb2d3d2d8
Uncle-Yuanl <1097304891@qq.com>
2022-03-24 16:39:23 +0800

    添加DataGenerator基类

--- modules/snippets.py
+++ modules/snippets.py
@@ -85,6 +85,118 @@ def orthogonally_resize(a, new_shape, window=2):
     return a / np.linalg.norm(a) * a_norm
 
 
+class DataGenerator:
+    """数据生成器模板
+    """
+    def __init__(self, data, batch_size=32, buffer_size=None):
+        self.data = data
+        self.batch_size = batch_size
+        if hasattr(self.data, '__len__'):
+            self.steps = len(data) // self.batch_size
+            if len(self.data) % self.batch_size != 0:
+                self.steps += 1
+        else:
+            self.steps = None
+        self.buffer_size = buffer_size or self.batch_size * 1000
+
+    def __len__(self):
+        return self.steps
+
+    def sample(self, random=False):
+        """采样函数，每个样本同时返回一个is_end标记
+        """
+        if random:
+            if self.steps is None:
+                def generator():
+                    caches, isfull = [], False
+                    for d in self.data:
+                        caches.append(d)
+                        if isfull:
+                            i = np.random.randint(len(caches))
+                            yield caches.pop(i)
+                        else:
+                            if len(caches) == self.buffer_size:
+                                isfull = True
+                    # isfull=False, data总量小于buffer_size
+                    while caches:
+                        i = np.random.randint(len(caches))
+                        yield caches.pop(i)
+
+            # hasattr(self.data, '__len__') = True
+            else:
+                def generator():
+                    # 不重复乱序
+                    for i in np.random.permutation(len(self.data)):
+                        yield self.data[i]
+
+            data = generator()
+        else:
+            data = iter(self.data)
+
+        # TODO(如果没有__next__方法呢)
+        d_current = next(data)  # 先取一个 后面开始迭代
+        for d_next in data:
+            yield False, d_current
+            d_current = d_next
+        yield True, d_current
+
+    def __iter__(self, random=False):
+        raise NotImplementedError
+
+    def forfit(self, random=True):
+        # TODO(怎么停？？？？)
+        while True:
+            for d in self.__iter__(random):
+                yield d
+
+    def fortest(self, random=False):
+        while True:
+            for d in self.__iter__(random):
+                yield d[0]
+
+    def to_dataset(self, types, shapes, names=None, padded_batch=False):
+        """转为tf.data.Dataset格式
+        如果传入names的话，自动把数据包装成dict形式。
+        """
+        if names is None:
+            generator = self.forfit
+        else:
+            if is_string(names):
+                warps = lambda k, v: {k, v}
+            elif is_string(names[0]):
+                warps = lambda k, v: dict(zip(k, v))
+            else:
+                # k, v本身是多维的，结果就第0维上每个元素，搞个dict
+                # example：tuple(dict(zip(i, j)) for i, j in
+                #  zip([[1,2,3], [3,4]],
+                #     [['a', 'b', 'c'], ['c', 'd']]))
+                # ({1: 'a', 2: 'b', 3: 'c'}, {3: 'c', 4: 'd'})
+                warps = lambda k, v: tuple(
+                    dict(zip(i, j)) for i, j in zip(k, v)
+                )
+
+            def generator():
+                for d in self.forfit():
+                    yield warps(names, d)
+
+            types = warps(names, types)
+            shapes = warps(names. shapes)
+
+        if padded_batch:
+            dataset = tf.data.Dataset.from_generator(
+                generator, output_types=types
+            )
+            dataset = dataset.padded_batch(self.batch_size, shapes)
+
+        else:
+            dataset = tf.data.Dataset.from_generator(
+                generator, output_types=types, output_shapes=shapes
+            )
+            dataset = dataset.batch(self.batch_size)
+
+        return dataset
+
+
 class ViterbiDecoder:
     """Viterbi解码算法基类
     """
